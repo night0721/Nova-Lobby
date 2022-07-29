0622a8df55f85b39b86f7334edf0a33c93084b66
night0721 <oscarfu0721@gmail.com>
2022-07-29 17:35:12 +0100

    Testing

--- src/main/java/me/night0721/minigames/Lobby.java
+++ src/main/java/me/night0721/minigames/Lobby.java
@@ -8,7 +8,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Lobby extends JavaPlugin {
-
     @Override
     public void onEnable() {
         getConfig().options().copyDefaults();
